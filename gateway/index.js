597d2a8b7c05638d45ef0423508af40eb921dc2c
Logesh <logeshc18022002@gmail.com>
2025-01-03 18:35:50 +0530

    token-removed

--- gateway/index.js
+++ gateway/index.js
@@ -17,5 +17,3 @@ app.use('/comment', authenticate, proxy('http://localhost:3002/'));
 app.listen(8000 , ()=> {
     console.log('Running on 8000')
 })
-
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NzM2YjE2YTU2ODg2NTIyYzQ3NjcwNiIsImVtYWlsIjoibG9nZXNoY0BleGFtcGxlLmNvbSIsImlhdCI6MTczNTYxNzM4NCwiZXhwIjoxNzM1NjIwOTg0fQ.x0hjSwMbQGrAVSSUmqwdEZn5SOmI9j-1WgH2doPR918
